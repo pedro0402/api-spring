7fec2c77387b6c24912f8760cb77b1df1e12d4b2
Pedro Moraes <pedro693.ph@gmail.com>
2025-03-23 15:17:41 -0300

    feat: criação da entidade Client

---
+++ src/main/java/com/pedro/jpa/libraryapi/model/Client.java
@@ -0,0 +1,25 @@
+package com.pedro.jpa.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String clientId;
+
+    private String clientSecret;
+
+    private String redirectURI;
+
+    private String scope;
+
+}
