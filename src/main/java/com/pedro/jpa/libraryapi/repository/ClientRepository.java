e3bd6a05ba94e7895059d0c2f31c56c43e5d6d64
Pedro Moraes <pedro693.ph@gmail.com>
2025-03-23 15:19:43 -0300

    feat: Criação dainterface ClientRepository

---
+++ src/main/java/com/pedro/jpa/libraryapi/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.pedro.jpa.libraryapi.repository;
+
+import com.pedro.jpa.libraryapi.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+}
