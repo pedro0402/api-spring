d66b93597a74d3f761ff29fde6c719a150f2bef4
Pedro Moraes <pedro693.ph@gmail.com>
2025-03-23 16:23:22 -0300

    ♻️  refactor: REfatorando a classe ClientService e adicionando a criptografia de senha do Client

--- src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
+++ src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
@@ -3,6 +3,7 @@ package com.pedro.jpa.libraryapi.service;
 import com.pedro.jpa.libraryapi.model.Client;
 import com.pedro.jpa.libraryapi.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,16 @@ import org.springframework.stereotype.Service;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public Client salvar(Client client){
+    public Client salvar(Client client) {
+        //seria ideal criar uma validação de cadastro de clients aqui
+        String senha = client.getClientSecret();
+        client.setClientSecret(passwordEncoder.encode(senha)); //criando a senha criptografada no banco
         return clientRepository.save(client);
     }
 
-    public Client obterPorClientId(String clientId){
+    public Client obterPorClientId(String clientId) {
         return clientRepository.findByClientId(clientId);
     }
 
