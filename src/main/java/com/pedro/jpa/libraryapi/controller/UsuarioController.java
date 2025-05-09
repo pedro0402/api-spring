6c3f8f8a6ebb8e732edd8efe8e6175c99effb0da
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-08 23:25:27 -0300

    feat: adiconando documentação via swagger na classe UsuarioController

--- src/main/java/com/pedro/jpa/libraryapi/controller/UsuarioController.java
+++ src/main/java/com/pedro/jpa/libraryapi/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import com.pedro.jpa.libraryapi.dto.UsuarioDTO;
 import com.pedro.jpa.libraryapi.mappers.UsuarioMapper;
 import com.pedro.jpa.libraryapi.model.Usuario;
 import com.pedro.jpa.libraryapi.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/usuarios")
 @RequiredArgsConstructor
+@Tag(name = "Usuários")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
@@ -19,6 +22,7 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Salvar", description = "Salvar um usuário")
     public void salvar(@RequestBody @Valid UsuarioDTO dto){
         Usuario entity = usuarioMapper.toEntity(dto);
         usuarioService.salvar(entity);
