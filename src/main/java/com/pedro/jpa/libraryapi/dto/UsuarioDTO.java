546e249f412273aae3ca0b2c4b2aa8b803c1b920
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-08 23:07:51 -0300

    feat: utilizando annotation do openapi para mudar o nome do DTO no swagger ui

--- src/main/java/com/pedro/jpa/libraryapi/dto/UsuarioDTO.java
+++ src/main/java/com/pedro/jpa/libraryapi/dto/UsuarioDTO.java
@@ -1,10 +1,12 @@
 package com.pedro.jpa.libraryapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
+@Schema(name = "Usuario")
 public record UsuarioDTO(
         @NotBlank(message = "Campo obrigatório")
         String login,
