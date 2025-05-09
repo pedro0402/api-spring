68a40b1167f9884c3664de859de489ef8289c03c
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-08 23:24:29 -0300

    feat: adiconando documentação via swagger na classe ClientController

--- src/main/java/com/pedro/jpa/libraryapi/controller/ClientController.java
+++ src/main/java/com/pedro/jpa/libraryapi/controller/ClientController.java
@@ -2,6 +2,9 @@ package com.pedro.jpa.libraryapi.controller;
 
 import com.pedro.jpa.libraryapi.model.Client;
 import com.pedro.jpa.libraryapi.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,12 +18,15 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/clients")
+@Tag(name = "Clients")
 public class ClientController implements GenericController {
 
     private final ClientService clientService;
 
     @PostMapping
     @PreAuthorize("hasRole('GERENTE')")
+    @Operation(summary = "Salvar", description = "Salvar um client")
+    @ApiResponse(responseCode = "201", description = "Client cadastrado com sucesso")
     public ResponseEntity<Void> salvar(@RequestBody Client client) {
         //não é o ideal cadastrar logo a entidade. o ideal seria criar um dto e dps mapea-lo.
         clientService.salvar(client);
