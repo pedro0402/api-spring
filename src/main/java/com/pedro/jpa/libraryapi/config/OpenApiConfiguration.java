bdefb683d08bf1e505a7858908f1e68133f789d6
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-08 22:43:56 -0300

    feat: Adicionando documentação via swagger na classe OpenApiConfiguration para ativar a autenticação no swagger ui

--- src/main/java/com/pedro/jpa/libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/com/pedro/jpa/libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package com.pedro.jpa.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,6 +17,17 @@ import org.springframework.context.annotation.Configuration;
                         name = "Pedro Moraes",
                         url = "https://github.com/pedro0402"
                 )
-        ))
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfiguration {
 }
