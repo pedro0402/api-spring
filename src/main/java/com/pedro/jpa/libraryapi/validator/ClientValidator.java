e1f180f30cbc6efb337dc5d647adc6d900a7dab2
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-10 16:13:39 -0300

    feat: Criando classe com lógica para validação de Client

---
+++ src/main/java/com/pedro/jpa/libraryapi/validator/ClientValidator.java
@@ -0,0 +1,36 @@
+package com.pedro.jpa.libraryapi.validator;
+
+import com.pedro.jpa.libraryapi.exceptions.RegistroDuplicadoException;
+import com.pedro.jpa.libraryapi.model.Client;
+import com.pedro.jpa.libraryapi.repository.ClientRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ClientValidator {
+
+    private final ClientRepository clientRepository;
+
+    public ClientValidator(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void validate(Client client){
+        if (clientAlreadyExists(client)){
+            throw new RegistroDuplicadoException("Client is already registered");
+        }
+        clientAlreadyExists(client);
+    }
+
+    private boolean clientAlreadyExists(Client client){
+        Optional<Client> clientFound = clientRepository.findByClientId(client.getClientId());
+
+        if (client.getId() == null){
+            return clientFound.isPresent();
+        }
+
+        return !client.getClientId().equals(clientFound.get().getClientId());
+    }
+
+}
