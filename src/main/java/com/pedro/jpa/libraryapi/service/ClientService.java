5c065fd02bc3ff1a5aad33ff6737055d98f1b252
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-10 16:14:54 -0300

    feat: adicionando ClientValidator no service e chamando a função validate no método salvar para fazer a validação do Client

--- src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
+++ src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
@@ -2,6 +2,7 @@ package com.pedro.jpa.libraryapi.service;
 
 import com.pedro.jpa.libraryapi.model.Client;
 import com.pedro.jpa.libraryapi.repository.ClientRepository;
+import com.pedro.jpa.libraryapi.validator.ClientValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,10 @@ public class ClientService {
 
     private final ClientRepository clientRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ClientValidator clientValidator;
 
     public Client salvar(Client client) {
-        //seria ideal criar uma validação de cadastro de clients aqui
+        clientValidator.validate(client);
         String senha = client.getClientSecret();
         client.setClientSecret(passwordEncoder.encode(senha)); //criando a senha criptografada no banco
         return clientRepository.save(client);
