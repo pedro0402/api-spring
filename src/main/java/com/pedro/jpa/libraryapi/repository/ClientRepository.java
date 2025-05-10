bda828565c982cc39a2ccb5f6f9827fd07e27002
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-10 15:53:00 -0300

    feat: adicionando Optional no findByClientId

--- src/main/java/com/pedro/jpa/libraryapi/repository/ClientRepository.java
+++ src/main/java/com/pedro/jpa/libraryapi/repository/ClientRepository.java
@@ -3,8 +3,10 @@ package com.pedro.jpa.libraryapi.repository;
 import com.pedro.jpa.libraryapi.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
-    Client findByClientId(String clientId);
+    Optional<Client> findByClientId(String clientId);
+
 }
