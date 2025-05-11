af7958b7ca954dfdf2ccea3da49acae0b4aed84b
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-11 00:24:33 -0300

    fix: resolvendo bug na lógica do ClientValidator

--- src/main/java/com/pedro/jpa/libraryapi/validator/ClientValidator.java
+++ src/main/java/com/pedro/jpa/libraryapi/validator/ClientValidator.java
@@ -1,33 +1,34 @@
 package com.pedro.jpa.libraryapi.validator;
 
+
 import com.pedro.jpa.libraryapi.exceptions.RegistroDuplicadoException;
 import com.pedro.jpa.libraryapi.model.Client;
 import com.pedro.jpa.libraryapi.repository.ClientRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-@RequiredArgsConstructor
 public class ClientValidator {
-
     private final ClientRepository clientRepository;
-        
-    public void validate(Client client){
-        if (clientAlreadyExists(client)){
+
+    public ClientValidator(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void validate(Client client) {
+        if (clientAlreadyExists(client)) {
             throw new RegistroDuplicadoException("Client is already registered");
         }
+        clientAlreadyExists(client);
     }
 
-    private boolean clientAlreadyExists(Client client){
+    private boolean clientAlreadyExists(Client client) {
         Optional<Client> clientFound = clientRepository.findByClientId(client.getClientId());
 
-        if (client.getId() == null){
+        if (client.getId() == null) {
             return clientFound.isPresent();
         }
-
-        return !client.getClientId().equals(clientFound.get().getClientId());
+        return clientFound.isPresent() && !clientFound.get().getId().equals(client.getId());
     }
-
 }
