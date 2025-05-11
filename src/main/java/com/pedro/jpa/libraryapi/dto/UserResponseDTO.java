0fce3407853f4f4f1700e61b21ae8c88c52dc7f2
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-11 13:00:26 -0300

    feat: criação da classe UserResponseDTO

---
+++ src/main/java/com/pedro/jpa/libraryapi/dto/UserResponseDTO.java
@@ -0,0 +1,15 @@
+package com.pedro.jpa.libraryapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserResponseDTO(
+        @NotBlank(message = "Campo obrigatório")
+        String login,
+        @NotBlank(message = "Campo obrigatório")
+        String senha,
+        @Email
+        @NotBlank(message = "Campo obrigatório")
+        String email
+) {
+}
