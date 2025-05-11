57a17eceec76ad90a6ff0c16a61e4d9edd9bc7ca
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-10 23:59:11 -0300

    refactor: Removendo injeção de dependência via construtor explicito e adicionando via RequiredArgsConstructor

--- src/main/java/com/pedro/jpa/libraryapi/validator/ClientValidator.java
+++ src/main/java/com/pedro/jpa/libraryapi/validator/ClientValidator.java
@@ -3,19 +3,17 @@ package com.pedro.jpa.libraryapi.validator;
 import com.pedro.jpa.libraryapi.exceptions.RegistroDuplicadoException;
 import com.pedro.jpa.libraryapi.model.Client;
 import com.pedro.jpa.libraryapi.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class ClientValidator {
 
     private final ClientRepository clientRepository;
-
-    public ClientValidator(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
-
+        
     public void validate(Client client){
         if (clientAlreadyExists(client)){
             throw new RegistroDuplicadoException("Client is already registered");
