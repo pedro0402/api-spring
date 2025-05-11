80207938ba96e9869d968c55c79c5935f295196e
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-10 23:45:47 -0300

    feat: adicionando imports do código

--- src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
+++ src/main/java/com/pedro/jpa/libraryapi/service/ClientService.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
