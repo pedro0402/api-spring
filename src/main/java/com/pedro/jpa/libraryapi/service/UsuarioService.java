367d305a17e68f32412533f5b95f89fc5a3aefd1
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-11 00:53:57 -0300

    feat: adicionando a classe de validação no meu service e adicionando validação ao salvar usuário

--- src/main/java/com/pedro/jpa/libraryapi/service/UsuarioService.java
+++ src/main/java/com/pedro/jpa/libraryapi/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.pedro.jpa.libraryapi.service;
 
 import com.pedro.jpa.libraryapi.model.Usuario;
 import com.pedro.jpa.libraryapi.repository.UsuarioRepository;
+import com.pedro.jpa.libraryapi.validator.UsuarioValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,10 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UsuarioValidator usuarioValidator;
 
     public void salvar(Usuario usuario) {
+        usuarioValidator.validate(usuario);
         String senha = usuario.getSenha();
         usuario.setSenha(passwordEncoder.encode(senha));
         usuarioRepository.save(usuario);
