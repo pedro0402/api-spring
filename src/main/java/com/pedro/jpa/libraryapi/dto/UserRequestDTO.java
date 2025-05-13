decd9f1191b2aac3457669b69228ab76d0c0a046
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-13 19:09:24 -0300

    fix: alterando o nome do DTO para UserRequestDTO, pois faz mais sentido baseado na aplicação

--- src/main/java/com/pedro/jpa/libraryapi/dto/UserRequestDTO.java
+++ src/main/java/com/pedro/jpa/libraryapi/dto/UserRequestDTO.java
@@ -3,7 +3,7 @@ package com.pedro.jpa.libraryapi.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserResponseDTO(
+public record UserRequestDTO(
         @NotBlank(message = "Campo obrigatório")
         String login,
         @NotBlank(message = "Campo obrigatório")
