baba61198e0586896948e5764d320dd243c9aacf
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-13 22:39:14 -0300

    chore: adiciona slf4j para logs no GlobalExceptionHandler

--- src/main/java/com/pedro/jpa/libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/pedro/jpa/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.pedro.jpa.libraryapi.dto.ErroResposta;
 import com.pedro.jpa.libraryapi.exceptions.CampoInvalidoException;
 import com.pedro.jpa.libraryapi.exceptions.OperacaoNaoPermitidaException;
 import com.pedro.jpa.libraryapi.exceptions.RegistroDuplicadoException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
@@ -17,10 +18,12 @@ import java.util.List;
 
 @RestControllerAdvice
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Erro de validação: {}", e.getMessage());
         List<FieldError> fieldErrors = e.getFieldErrors();
         List<ErroCampo> listaErrors = fieldErrors
                 .stream()
@@ -57,6 +60,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErroResposta handleErrosNaoTratados(RuntimeException e) {
+        log.error("Ocorreu um erro inesperado: {}", e);
         return new ErroResposta(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro inesperado.", List.of());
     }
 
