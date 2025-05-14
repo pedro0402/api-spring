0172ce01473063d8a6afba120aa43a45641b0cb8
Pedro Moraes <pedro693.ph@gmail.com>
2025-05-14 16:58:36 -0300

    chore: adiciona logs na aplicação

--- src/main/java/com/pedro/jpa/libraryapi/config/DatabaseConfiguration.java
+++ src/main/java/com/pedro/jpa/libraryapi/config/DatabaseConfiguration.java
@@ -2,6 +2,7 @@ package com.pedro.jpa.libraryapi.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DatabaseConfiguration {
 
     @Value("${spring.datasource.url}")
@@ -36,6 +38,7 @@ public class DatabaseConfiguration {
 
     @Bean
     public DataSource hikariDataSource(){
+        log.info("Iniciando conexão com banco na URL: {}", url);
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(url);
         config.setUsername(username);
